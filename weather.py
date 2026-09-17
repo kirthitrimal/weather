21b6fada0a0894105f7a618d000294397d415482
kirthi <kirthitrimal@gmail.com>
2026-09-17 14:35:21 +0530

    Added the conversion of fahrenheit

--- weather.py
+++ weather.py
@@ -4,4 +4,7 @@ if degree <=26:
 elif degree >26 and degree <=40:
     print("Normal weather stay fit")
 else:
-    print("Hot! Weather! drink some water")        
+    print("Hot! Weather! drink some water")
+fahrenheit=((degree*1.8)+32)
+print("the fahrenheit value is",fahrenheit,"F")    
+
